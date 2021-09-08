3114ef6007609b7dd9c671db0034b331b4821093
Claudio Alves <claudio.alves@monitoratec.com.br>
2021-09-08 10:36:09 -0300

    Exibindo detalhes das orders.

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,16 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({Key? key, required this.order}) : super(key: key);
 
   @override
+  _OrderWidgetState createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(DateFormat('dd/mm/yyyy hh:mm').format(order.date))
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/mm/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: Icon(Icons.expand_more)),
+          ),
+          if (_expanded)
+            Container(
+              height: (widget.order.products.length * 20) + 8,
+              padding: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('${product.quantity}x ${product.name}'),
+                          Text('${product.price}')
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+        ],
       ),
     );
   }
